a551c22779c59df43820dc0b003dc5d1668406df
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-09-02 12:17:31 +0300

    Scala3LanguageSubstitutor: avoid NPE, detect dotty 0.18.x #SCL-16158
    NPE can occur for files of sub-projects or for scala files placed
    outside default sources folder (src/main/scala)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/Scala3LanguageSubstitutor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/Scala3LanguageSubstitutor.scala
@@ -6,6 +6,8 @@ import com.intellij.lang.Language
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.LanguageSubstitutor
+import org.jetbrains.plugins.scala.extensions.{NullSafe, ObjectExt}
+import org.jetbrains.sbt.RichOption
 
 final class Scala3LanguageSubstitutor extends LanguageSubstitutor {
 
@@ -15,17 +17,20 @@ final class Scala3LanguageSubstitutor extends LanguageSubstitutor {
 
     virtualFile.getExtension match {
       case `scalaExtension` if parent != null =>
-        val bytes = parent
-          .getParent
-          .getParent
-          .getParent
-          .findChild("build.sbt")
-          .contentsToByteArray
+        val projectRoot = parent
+          .getParent.toOption
+          .safeMap(_.getParent)
+          .safeMap(_.getParent)
+        val buildSbtContent = projectRoot
+          .safeMap(_.findChild("build.sbt"))
+          .safeMap(file => new String(file.contentsToByteArray))
 
-        if (new String(bytes).contains("0.17.0"))
-          Scala3Language.INSTANCE
-        else
-          ScalaLanguage.INSTANCE
+        buildSbtContent match {
+          case Some(contents) if contents.contains("0.17.") || contents.contains("0.18.") =>
+            Scala3Language.INSTANCE
+          case None =>
+            ScalaLanguage.INSTANCE
+        }
       case _ => null
     }
   }
